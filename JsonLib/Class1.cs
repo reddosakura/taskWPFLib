6deaf1b89c4762c254bdb3b20ca7af76f6e7c577
reddosakura <alexander05511@gmail.com>
2023-06-02 21:47:17 +0300

    Добавьте файлы проекта.

---
+++ JsonLib/Class1.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace JsonLib
+{
+    public class JsonUtils
+    {
+        private static string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        public static void Deserial<T>(T obj, string file)
+        {
+            string json = JsonConvert.SerializeObject(obj);
+            File.WriteAllText(path + "\\" + file, json);
+        }
+        public static T Serial<T>(string file)
+        {
+            if (File.Exists(path + "\\" + file))
+            {
+                string json = File.ReadAllText(path + "\\" + file);
+                T obj = JsonConvert.DeserializeObject<T>(json);
+                return obj;
+            }
+            return default(T);
+        }
+    }
+}
